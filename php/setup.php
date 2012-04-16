40e895682ddef4b10a757be5b50115065c797ed3
bram <b@bram.ws>
2012-04-16 14:55:55 +0200

    add standard mysql setup

--- php/setup.php
+++ php/setup.php
@@ -69,7 +69,7 @@ define("__CA_DB_USER__", 'admin');
 define("__CA_DB_PASSWORD__", 'my_database_password');
 
 # The name of your CollectiveAccess database
-define("__CA_DB_DATABASE__", 'collecitiveaccess');
+define("__CA_DB_DATABASE__", 'collectiveaccess');
 
 # Only MySQL databases are currently supported so leave this set to 'mysql'!
 define("__CA_DB_TYPE__", 'mysql');
